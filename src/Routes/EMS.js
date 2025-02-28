76874e64b5640225f5dc52d85eb861a8cd7bd9b8
parkchanseam <ased1233@naver.com>
2025-02-28 10:00:32 +0900

    update EMS backGround#2

--- src/Routes/EMS.js
+++ src/Routes/EMS.js
@@ -24,9 +24,9 @@ const Wrapper = styled.div`
 
 const Headerclass = styled.div`
   //   position: absolute;
-  height: 90px;
-  max-width: 1920px;
-  margin: auto;
+  // height: 90px;
+  // max-width: 1920px;
+  // margin: auto;
 `;
 
 const EmsWrapper = styled.div`
